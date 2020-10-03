2f8ffef8b3333d0a9d2335e6b5c4bd86159cf097
air <airmanhigh@gmail.com>
2020-10-04 00:27:35 +0800

    font: use wqy-microhei

--- theme.lua
+++ theme.lua
@@ -11,7 +11,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "Roboto 9" -- "sans 8"
+theme.font          = "文泉驿微米黑 10" -- "sans 8"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
