b57e5ff174c7c1c383c13f5c90de6e29ee1c7313
air <airmanhigh@gmail.com>
2021-02-01 14:08:43 +0800

    feat: add several new apps to menu
    These new apps are:
    - wps-office
    - pycharm-community-edition
    - deepin-wine-tim
    - steam
    
    And add a missing entry to README file:
    - deepin-wine-wechat
    
    Add a new menu:
    - Social

--- my_menu.lua
+++ my_menu.lua
@@ -21,7 +21,11 @@ require("awful.hotkeys_popup.keys")
 local my_menu = {}
 
 local tool_menu = {
-    {"File Manager", "nautilus"}
+    {"File Manager", "nautilus"},
+    {"WPS Writer", "env LANG=zh_CN.UTF-8 wps"},
+    {"WPS Spreadsheets", "env LANG=zh_CN.UTF-8 et"},
+    {"WPS Presentation", "env LANG=zh_CN.UTF-8 wpp"},
+    {"WPS PDF", "env LANG=zh_CN.UTF-8 wpspdf"}
 }
 
 local awesome_menu = {
@@ -53,18 +57,23 @@ local develop_menu = {
     {"Emacs", "emacs"},
     {"Zathura", "zathura"},
     {"VS Code", "code"},
-    {"CLion", "clion"}
+    {"CLion", "clion"},
+    {"Pycharm", "pycharm"}
+}
+
+local social_menu = {
+    {"Telegram", "telegram-desktop"},
+    {"Wechat", "/opt/apps/com.qq.weixin.deepin/files/run.sh"}
 }
 
 local other_menu = {
-    {"deepin-wine-wechat", "/opt/apps/com.qq.weixin.deepin/files/run.sh"},
-    {"Telegram", "telegram-desktop"},
     {"Nextcloud", "nextcloud"},
     {"Spotify", "spotify"},
     {"Baidu Net Disk", "/usr/lib/baidunetdisk/baidunetdisk"},
     {"Flame shot", "flameshot"},
     {"fcitx5-config-qt", "fcitx5-config-qt"},
-    {"font-manager", "font-manager"}
+    {"font-manager", "font-manager"},
+    {"Steam", "steam"}
 }
 
 my_menu.main_menu =
@@ -75,6 +84,7 @@ my_menu.main_menu =
             {"Tools", tool_menu},
             {"Network", network_menu},
             {"Develop", develop_menu},
+            {"Social", social_menu},
             {"Other", other_menu},
             {"Open Terminal", terminal}
         }
